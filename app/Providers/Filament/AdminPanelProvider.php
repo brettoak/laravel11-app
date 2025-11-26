bfb6c3f6137622b4b002ade0de8eff1cb6282d91
Brett <12744007+brettoak@users.noreply.github.com>
2025-11-26 23:13:45 +1100

    Add Filament config and update admin panel navigation
    Introduced a new config/filament.php configuration file for Filament settings. Updated AdminPanelProvider to add 'Single Job' and 'Multiple Jobs' navigation items under 'Task Management', made the sidebar collapsible on desktop, and commented out the FilamentInfoWidget. Also commented out the '/admin' route in web.php.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -6,6 +6,7 @@ use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\NavigationItem;
 use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -31,6 +32,19 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->navigationItems([
+                NavigationItem::make('Single Job')
+                    ->url('/reverb/single/job', shouldOpenInNewTab: false)
+                    ->icon('heroicon-o-presentation-chart-line')
+                    ->group('Task Management')
+                    ->sort(1),
+                NavigationItem::make('Multiple Jobs')
+                    ->url('/reverb/multiple/jobs', shouldOpenInNewTab: false)
+                    ->icon('heroicon-o-presentation-chart-line')
+                    ->group('Task Management')
+                    ->sort(2),
+            ])
+            ->sidebarCollapsibleOnDesktop()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
@@ -39,7 +53,7 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
                 AccountWidget::class,
-                FilamentInfoWidget::class,
+                // FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
