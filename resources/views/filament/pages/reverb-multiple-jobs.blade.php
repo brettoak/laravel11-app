d3b45b1a8c143474ba5c8ec53dd881a216ba551f
Brett <handongyu168@163.com>
2025-12-28 19:36:02 +1100

    Expand container width to full in Filament pages

--- resources/views/filament/pages/reverb-multiple-jobs.blade.php
+++ resources/views/filament/pages/reverb-multiple-jobs.blade.php
@@ -3,7 +3,7 @@
         @vite(['resources/js/app.js'])
     @endpush
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-8">
+        <div class="max-w-full mx-auto sm:px-6 lg:px-8 space-y-8">
             @livewire('reverb-multiple-jobs')
         </div>
     </div>
--- resources/views/filament/pages/upload-spread-sheet.blade.php
+++ resources/views/filament/pages/upload-spread-sheet.blade.php
@@ -3,7 +3,7 @@
         @vite(['resources/js/app.js'])
     @endpush
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-full mx-auto sm:px-6 lg:px-8">
             @livewire('upload-spread-sheet')
         </div>
     </div>
